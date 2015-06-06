413bd2aa5a73ee521cf7c32c905dd77d43f99674
Henry Thiemann <heenryt@gmail.com>
2015-06-06 12:23:29 -0700

    added node_home, OrbGameScene

---
+++ Pulse2/OrbGameScene.h
@@ -0,0 +1,34 @@
+//
+//  SongTrainScene.h
+//  protogame191
+//
+//  Created by Ben McK on 5/5/15.
+//  Copyright (c) 2015 Henry Thiemann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import "LoopData.h"
+#import "Conductor.h"
+
+@interface OrbGameScene : SKScene
+
+- (instancetype)initWithLoopData:(LoopData *)data conductor:(Conductor *)conductor size:(CGSize)size;
+
+@property Conductor *conductor;
+@property LoopData *loopData;
+@property double nextBeat;
+@property BOOL resetLoopBeat;
+@property BOOL trainIsJumping;
+@property double resetLoopTime;
+@property double lastBeat;
+@property SKSpriteNode *train;
+@property int streakCounter;
+@property SKLabelNode *streakDisplay;
+@property CGFloat leftTrackCenter;
+@property CGFloat rightTrackCenter;
+@property SystemSoundID hihatSound;
+
+- (void)displayDirections;
+
+@end
